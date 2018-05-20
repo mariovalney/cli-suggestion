0255912a9f5ce10e063b5987da43e162602ef1fb
Mário Valney <mario@vizir.com.br>
2018-05-19 22:56:35 -0300

    [Feat] Module accepts options and return a funcion to run suggestions

--- index.js
+++ index.js
@@ -1,26 +1,56 @@
 'use strict';
 
-function createSuggestion(suggestion, tries) {
-  return {
-    suggestion: suggestion,
-    tries: tries
+function run(opts) {
+  var options = {
+    text: 'Did want to say that?',
+    command: ''
   };
-}
 
-function run(command) {
-  const suggestions = [
-    createSuggestion('--version', [ 'version', 'v' ])
-  ];
-
-  for (var i = 0; i < suggestions.length; i++) {
-    for(var j = 0; j < suggestions[i].tries.length; j++) {
-      if (suggestions[i].tries[j] != command) continue;
-
-      return 'Did want to say that?' + "\n" + "    " + suggestions[i].suggestion;
-    }
+  if (typeof opts == 'string') {
+    opts = {
+      command: opts
+    };
   }
 
-  return false;
+  if (typeof opts !== 'object') {
+    opts = {};
+  }
+
+  options.text = opts.text || options.text;
+  options.command = opts.command || options.command;
+
+  function createSuggestion(suggestion) {
+    var command = options.command;
+    if (options.command) command += ' ';
+
+    return options.text + "\n" + '    ' + command + suggestion;
+  }
+
+  function createSuggestionArray(suggestion, tries) {
+    return {
+      suggestion: suggestion,
+      tries: tries
+    };
+  }
+
+  function doSuggestion(command) {
+    const suggestions = [
+      createSuggestionArray('--test', [ 'test' ])
+    ];
+
+    for (var i = 0; i < suggestions.length; i++) {
+      for(var j = 0; j < suggestions[i].tries.length; j++) {
+        if (suggestions[i].tries[j] != command) continue;
+
+        return createSuggestion(suggestions[i].suggestion);
+      }
+    }
+
+    return false;
+  }
+
+  return doSuggestion;
 }
+
 
 module.exports = run;
