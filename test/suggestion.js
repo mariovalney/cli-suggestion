b712da66ea714744fca75373d4aed5897f3cb623
Mário Valney <mario@vizir.com.br>
2018-05-19 22:56:12 -0300

    [Test] Added tests

--- test/suggestion.js
+++ test/suggestion.js
@@ -3,12 +3,49 @@
 var expect = require('expect');
 var suggestion = require('../');
 
+const suggested = '--test';
+
 describe('CLI Suggestion', function() {
 
-  it('should return suggestions', function(done) {
-    expect(suggestion('v')).toEqual('Did want to say that?' + "\n" + '    --version');
-    expect(suggestion('version')).toEqual('Did want to say that?' + "\n" + '    --version');
+  it('should return default suggestion text and default command', function(done) {
+    var doSuggestion = suggestion();
 
+    expect(doSuggestion('test')).toEqual('Did want to say that?' + "\n" + '    ' + suggested);
+    done(0);
+  });
+
+  it('should return default suggestion text and command if given as string', function(done) {
+    var doSuggestion = suggestion('command');
+
+    expect(doSuggestion('test')).toEqual('Did want to say that?' + "\n" + '    command ' + suggested);
+    done(0);
+  });
+
+  it('should return default suggestion text and command if given as object', function(done) {
+    var doSuggestion = suggestion({
+      command: 'command'
+    });
+
+    expect(doSuggestion('test')).toEqual('Did want to say that?' + "\n" + '    command ' + suggested);
+    done(0);
+  });
+
+  it('should return suggestion text and default command', function(done) {
+    var doSuggestion = suggestion({
+      text: 'Você quis dizer?'
+    });
+
+    expect(doSuggestion('test')).toEqual('Você quis dizer?' + "\n" + '    ' + suggested);
+    done(0);
+  });
+
+  it('should return suggestion text and command if booth given', function(done) {
+    var doSuggestion = suggestion({
+      text: 'Você quis dizer?',
+      command: 'command'
+    });
+
+    expect(doSuggestion('test')).toEqual('Você quis dizer?' + "\n" + '    command ' + suggested);
     done(0);
   });
 
